ca14cf901992b2c46b7bcd358e4ec81b1496b9a6
Cloris5 <994837979@qq.com>
2020-05-31 18:59:07 +0800

    2021

--- Online_Game_Final_Project/Assets/Hard Surface Pro/Standard Assets/Image Effects (Pro Only)/SSAOEffect.cs
+++ Online_Game_Final_Project/Assets/Hard Surface Pro/Standard Assets/Image Effects (Pro Only)/SSAOEffect.cs
@@ -72,7 +72,6 @@ public class SSAOEffect : MonoBehaviour
 	}
 	
 	void OnEnable () {
-		GetComponent<Camera>().depthTextureMode |= DepthTextureMode.DepthNormals;
 	}
 
 	private void CreateMaterials ()
@@ -101,11 +100,6 @@ public class SSAOEffect : MonoBehaviour
 
 		// Render SSAO term into a smaller texture
 		RenderTexture rtAO = RenderTexture.GetTemporary (source.width / m_Downsampling, source.height / m_Downsampling, 0);
-		float fovY = GetComponent<Camera>().fieldOfView;
-		float far = GetComponent<Camera>().farClipPlane;
-		float y = Mathf.Tan (fovY * Mathf.Deg2Rad * 0.5f) * far;
-		float x = y * GetComponent<Camera>().aspect;
-		m_SSAOMaterial.SetVector ("_FarCorner", new Vector3(x,y,far));
 		int noiseWidth, noiseHeight;
 		if (m_RandomTexture) {
 			noiseWidth = m_RandomTexture.width;
