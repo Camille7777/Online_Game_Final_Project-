ec35c7c05b2aa7b5bb49a1e7ca20c5274822314b
Cloris5 <994837979@qq.com>
2020-05-31 19:44:42 +0800

    fdg

--- Online_Game_Final_Project/Assets/Scripts/LaunchManager.cs
+++ Online_Game_Final_Project/Assets/Scripts/LaunchManager.cs
@@ -94,7 +94,7 @@ public class LaunchManager : MonoBehaviourPunCallbacks
     public override void OnJoinedRoom()
     {
         Debug.Log(PhotonNetwork.NickName + " joined to " + PhotonNetwork.CurrentRoom.Name);
-        PhotonNetwork.LoadLevel("Level_1");
+        PhotonNetwork.LoadLevel("level test");
     }
 
     public override void OnPlayerEnteredRoom(Player newPlayer)
