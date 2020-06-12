1aa4db68f18dd5423ea0238f24299e664854d7a6
Chris <SWE1709229@xmu.edu.my>
2020-06-12 21:35:07 +0800

    Friction

--- Online_Game_Final_Project/Assets/Scripts/WaveGenerator.cs
+++ Online_Game_Final_Project/Assets/Scripts/WaveGenerator.cs
@@ -6,13 +6,16 @@ public class WaveGenerator : MonoBehaviour {
 	public float amplitude = 2;
 	public float frequency = 2;
 	public float startMoving = 0;
-	public CharacterController character;
+	private Rigidbody rb;
+	
+
 
 	public char xyz = 'x';
 	float angle = 0;
 	float original = 0;
-	public Vector3 m_velocity;
+	
 	void Start(){
+		rb = GetComponent<Rigidbody>();
 		if(xyz=='x')
 			original = transform.position.x;
 		else if (xyz=='y')
@@ -27,19 +30,20 @@ public class WaveGenerator : MonoBehaviour {
 	}
 
 	void move()
-    {
+	{
 		angle += Time.deltaTime * frequency;
 		Vector3 pos = transform.position;
 
 		if (xyz == 'x')
-			pos.x = original + Mathf.Sin(angle) * amplitude;
+		{
+			Debug.Log("HHHH");
+			rb.velocity = new Vector3(Mathf.Sin(angle) * amplitude, 0, 0);
+		}
+		//pos.x = original + Mathf.Sin(angle) * amplitude;
 		else if (xyz == 'y')
 			pos.y = original + Mathf.Sin(angle) * amplitude;
 		else
 			pos.z = original + Mathf.Sin(angle) * amplitude;
-
-		m_velocity = (transform.position - pos) / Time.fixedDeltaTime;
 		transform.position = pos;
-		character.attachedRigidbody.position = pos;
 	}
 }
